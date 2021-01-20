c818c5119c64301aec706460fed32a1b13ba5d6f
sultan-ongarbayev <sultan.ongarbayev@yahoo.com>
2021-01-20 22:50:35 +0100

    XML content type support added.

--- src/main/java/com/cleevio/watchshopservice/config/WebConfiguration.java
+++ src/main/java/com/cleevio/watchshopservice/config/WebConfiguration.java
@@ -1,9 +1,18 @@
 package com.cleevio.watchshopservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+import org.springframework.web.servlet.view.xml.MappingJackson2XmlView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Web application context configuration.
@@ -12,4 +21,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @ComponentScan(basePackages = "com.cleevio.watchshopservice.controller")
 public class WebConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public ViewResolver contentNegotiatingViewResolver() {
+        ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+
+        List<View> views = new ArrayList<>();
+        views.add(new MappingJackson2XmlView());
+        views.add(new MappingJackson2JsonView());
+
+        resolver.setDefaultViews(views);
+        return resolver;
+    }
 }
